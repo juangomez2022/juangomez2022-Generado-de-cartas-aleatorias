1e92925e3962247c6d6e961dbde67c354aa9888f
juangomez2022 <110196663+juangomez2022@users.noreply.github.com>
2026-03-19 17:39:31 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+	let palos = ["diamond", "heart", "spade", "club"];
+	let simbols = {
+		diamond: "♦",
+		heart: "♥",
+		spade: "♠",
+		club: "♣"
+	};
+
+	let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+	let paloRandom = palos[Math.floor(Math.random() * palos.length)];
+	let valorRandom = values[Math.floor(Math.random() * values.length)];
+
+	let top = document.getElementById("topSuit");
+	let number = document.getElementById("number");
+	let bottom = document.getElementById("bottomSuit");
+
+	top.innerHTML = simbols[paloRandom];
+	bottom.innerHTML = simbols[paloRandom];
+	number.innerHTML = valorRandom;
+
+	let colorRandom = Math.random() < 0.5 ? "red" : "black";
+
+	top.style.color = colorRandom;
+	bottom.style.color = colorRandom;
+
+	number.style.color = "black";
 };
